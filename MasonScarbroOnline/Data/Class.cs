43eac92c0180b1361855e364d45c1d01584e4a16
Mason Scarbro <120221711+MasonScarbro@users.noreply.github.com>
2026-07-25 17:04:53 -0400

    Added Context

---
+++ MasonScarbroOnline/Data/Class.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MasonScarbroOnline.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<MasonScarbroOnline.Models.Project> Projects { get; set; }
+    }
+}
